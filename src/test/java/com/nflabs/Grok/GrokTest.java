df34a899d896fb6f510af77d06451abd0eea4576
anthony corbacho <corbacho.anthony@gmail.com>
2014-03-06 17:19:59 +0900

    indent test case

--- src/test/java/com/nflabs/Grok/GrokTest.java
+++ src/test/java/com/nflabs/Grok/GrokTest.java
@@ -15,257 +15,259 @@ import com.nflabs.grok.Match;
 public class GrokTest {
 
 
-	/**
-	 * Do some basic test
-	 *
-	 * @throws Throwable
-	 */
-	/*public void testGrok() throws Throwable {
-		Grok g = new Grok();
-
-		g.addPatternFromFile("patterns/base");
-		g.compile("%{APACHE}");
-		Match gm = g.match("127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326");
-		//Match gm = g.match("10.192.1.47");
-		gm.captures();
-		//See the result
-		System.out.println(gm.toJson());
-
-	}*/
-	private Grok g = new Grok();
-
-	@Test
-	public void test00_basic(){
-		Grok g = new Grok();
-		boolean thrown = false;
-
-		// expected exception
-		try {
-			g.addPatternFromFile("/good/luck");
-		} catch (GrokException e) {
-			thrown = true;
-		}
-		assertTrue(thrown);
-
-		thrown = false;
-
-		try {
-			g.addPattern(null, "");
-		} catch (GrokException e) {
-			thrown = true;
-		}
-		assertTrue(thrown);
-
-		thrown = false;
-		try {
-			g.copyPatterns(null);
-		} catch (GrokException e) {
-			thrown = true;
-		}
-		assertTrue(thrown);
-
-		thrown = false;
-		try {
-			g.copyPatterns(new HashMap<String, String>());
-		} catch (GrokException e) {
-			thrown = true;
-		}
-		assertTrue(thrown);
-	}
-
-	@Test
-	public void test001_username() throws Throwable{
-
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{USERNAME}");
-
-		Match gm = g.match("root");
-		gm.captures();
-		assertEquals("{USERNAME=root}", gm.toMap().toString());
-
-		gm = g.match("r00t");
-		gm.captures();
-		assertEquals("{USERNAME=r00t}", gm.toMap().toString());
-
-		gm = g.match("guest");
-		gm.captures();
-		assertEquals("{USERNAME=guest}", gm.toMap().toString());
-
-		gm = g.match("guest1234");
-		gm.captures();
-		assertEquals("{USERNAME=guest1234}", gm.toMap().toString());
+  /**
+   * Do some basic test
+   *
+   * @throws Throwable
+   */
+  /*
+   * public void testGrok() throws Throwable { Grok g = new Grok();
+   *
+   * g.addPatternFromFile("patterns/base"); g.compile("%{APACHE}"); Match gm =
+   * g.match("127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326"
+   * ); //Match gm = g.match("10.192.1.47"); gm.captures(); //See the result
+   * System.out.println(gm.toJson());
+   *
+   * }
+   */
+  private Grok g = new Grok();
+
+  @Test
+  public void test00_basic() {
+    Grok g = new Grok();
+    boolean thrown = false;
+
+    // expected exception
+    try {
+      g.addPatternFromFile("/good/luck");
+    } catch (GrokException e) {
+      thrown = true;
+    }
+    assertTrue(thrown);
+
+    thrown = false;
+
+    try {
+      g.addPattern(null, "");
+    } catch (GrokException e) {
+      thrown = true;
+    }
+    assertTrue(thrown);
+
+    thrown = false;
+    try {
+      g.copyPatterns(null);
+    } catch (GrokException e) {
+      thrown = true;
+    }
+    assertTrue(thrown);
+
+    thrown = false;
+    try {
+      g.copyPatterns(new HashMap<String, String>());
+    } catch (GrokException e) {
+      thrown = true;
+    }
+    assertTrue(thrown);
+  }
+
+  @Test
+  public void test001_username() throws Throwable {
+
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{USERNAME}");
+
+    Match gm = g.match("root");
+    gm.captures();
+    assertEquals("{USERNAME=root}", gm.toMap().toString());
+
+    gm = g.match("r00t");
+    gm.captures();
+    assertEquals("{USERNAME=r00t}", gm.toMap().toString());
+
+    gm = g.match("guest");
+    gm.captures();
+    assertEquals("{USERNAME=guest}", gm.toMap().toString());
+
+    gm = g.match("guest1234");
+    gm.captures();
+    assertEquals("{USERNAME=guest1234}", gm.toMap().toString());
+
+    gm = g.match("john doe");
+    gm.captures();
+    assertEquals("{USERNAME=john}", gm.toMap().toString());
+  }
+
+  @Test
+  public void test001_username2() throws Throwable {
+
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{USER}");
+
+    Match gm = g.match("root");
+    gm.captures();
+    assertEquals("{USER=root}", gm.toMap().toString());
+
+    gm = g.match("r00t");
+    gm.captures();
+    assertEquals("{USER=r00t}", gm.toMap().toString());
+
+    gm = g.match("guest");
+    gm.captures();
+    assertEquals("{USER=guest}", gm.toMap().toString());
 
-		gm = g.match("john doe");
-		gm.captures();
-		assertEquals("{USERNAME=john}", gm.toMap().toString());
-	}
+    gm = g.match("guest1234");
+    gm.captures();
+    assertEquals("{USER=guest1234}", gm.toMap().toString());
 
-	@Test
-	public void test001_username2() throws Throwable{
+    gm = g.match("john doe");
+    gm.captures();
+    assertEquals("{USER=john}", gm.toMap().toString());
+  }
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{USER}");
+  @Test
+  public void test002_numbers() throws Throwable {
 
-		Match gm = g.match("root");
-		gm.captures();
-		assertEquals("{USER=root}", gm.toMap().toString());
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{NUMBER}");
 
-		gm = g.match("r00t");
-		gm.captures();
-		assertEquals("{USER=r00t}", gm.toMap().toString());
+    Match gm = g.match("-42");
+    gm.captures();
+    assertEquals("{NUMBER=-42}", gm.toMap().toString());
 
-		gm = g.match("guest");
-		gm.captures();
-		assertEquals("{USER=guest}", gm.toMap().toString());
+  }
 
-		gm = g.match("guest1234");
-		gm.captures();
-		assertEquals("{USER=guest1234}", gm.toMap().toString());
+  @Test
+  public void test003_word() throws Throwable {
 
-		gm = g.match("john doe");
-		gm.captures();
-		assertEquals("{USER=john}", gm.toMap().toString());
-	}
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{WORD}");
 
-	@Test
-	public void test002_numbers() throws Throwable{
+    Match gm = g.match("a");
+    gm.captures();
+    assertEquals("{WORD=a}", gm.toMap().toString());
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{NUMBER}");
+    gm = g.match("abc");
+    gm.captures();
+    assertEquals("{WORD=abc}", gm.toMap().toString());
 
-		Match gm = g.match("-42");
-		gm.captures();
-		assertEquals("{NUMBER=-42}", gm.toMap().toString());
+  }
 
-	}
+  @Test
+  public void test004_SPACE() throws Throwable {
 
-	@Test
-	public void test003_word() throws Throwable{
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{SPACE}");
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{WORD}");
+    Match gm = g.match("abc dc");
+    gm.captures();
+    assertEquals("{SPACE=}", gm.toMap().toString());
 
-		Match gm = g.match("a");
-		gm.captures();
-		assertEquals("{WORD=a}", gm.toMap().toString());
+  }
 
-		gm = g.match("abc");
-		gm.captures();
-		assertEquals("{WORD=abc}", gm.toMap().toString());
+  @Test
+  public void test005_NOTSPACE() throws Throwable {
 
-	}
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{NOTSPACE}");
 
-	@Test
-	public void test004_SPACE() throws Throwable{
+    Match gm = g.match("abc dc");
+    gm.captures();
+    assertEquals("{NOTSPACE=abc}", gm.toMap().toString());
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{SPACE}");
+  }
 
-		Match gm = g.match("abc dc");
-		gm.captures();
-		assertEquals("{SPACE=}", gm.toMap().toString());
+  @Test
+  public void test006_QUOTEDSTRING() throws Throwable {
 
-	}
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{QUOTEDSTRING:text}");
 
-	@Test
-	public void test005_NOTSPACE() throws Throwable{
+    Match gm = g.match("\"abc dc\"");
+    gm.captures();
+    assertEquals("{text=abc dc}", gm.toMap().toString());
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{NOTSPACE}");
+  }
 
-		Match gm = g.match("abc dc");
-		gm.captures();
-		assertEquals("{NOTSPACE=abc}", gm.toMap().toString());
+  @Test
+  public void test007_UUID() throws Throwable {
 
-	}
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{UUID}");
 
-	@Test
-	public void test006_QUOTEDSTRING() throws Throwable{
+    Match gm = g.match("61243740-4786-11e3-86a7-0002a5d5c51b");
+    gm.captures();
+    assertEquals("{UUID=61243740-4786-11e3-86a7-0002a5d5c51b}", gm.toMap().toString());
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{QUOTEDSTRING:text}");
+    gm = g.match("7F8C7CB0-4786-11E3-8F96-0800200C9A66");
+    gm.captures();
+    assertEquals("{UUID=7F8C7CB0-4786-11E3-8F96-0800200C9A66}", gm.toMap().toString());
 
-		Match gm = g.match("\"abc dc\"");
-		gm.captures();
-		assertEquals("{text=abc dc}", gm.toMap().toString());
+    gm = g.match("03A8413C-F604-4D21-8F4D-24B19D98B5A7");
+    gm.captures();
+    assertEquals("{UUID=03A8413C-F604-4D21-8F4D-24B19D98B5A7}", gm.toMap().toString());
 
-	}
+  }
 
-	@Test
-	public void test007_UUID() throws Throwable{
+  @Test
+  public void test008_MAC() throws Throwable {
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{UUID}");
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{MAC}");
 
-		Match gm = g.match("61243740-4786-11e3-86a7-0002a5d5c51b");
-		gm.captures();
-		assertEquals("{UUID=61243740-4786-11e3-86a7-0002a5d5c51b}", gm.toMap().toString());
+    Match gm = g.match("5E:FF:56:A2:AF:15");
+    gm.captures();
+    assertEquals("{MAC=5E:FF:56:A2:AF:15}", gm.toMap().toString());
 
-		gm = g.match("7F8C7CB0-4786-11E3-8F96-0800200C9A66");
-		gm.captures();
-		assertEquals("{UUID=7F8C7CB0-4786-11E3-8F96-0800200C9A66}", gm.toMap().toString());
+  }
 
-		gm = g.match("03A8413C-F604-4D21-8F4D-24B19D98B5A7");
-		gm.captures();
-		assertEquals("{UUID=03A8413C-F604-4D21-8F4D-24B19D98B5A7}", gm.toMap().toString());
+  @Test
+  public void test009_IPORHOST() throws Throwable {
 
-	}
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{IPORHOST}");
 
-	@Test
-	public void test008_MAC() throws Throwable{
+    Match gm = g.match("www.google.fr");
+    gm.captures();
+    assertEquals("{IPORHOST=www.google.fr}", gm.toMap().toString());
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{MAC}");
+  }
 
-		Match gm = g.match("5E:FF:56:A2:AF:15");
-		gm.captures();
-		assertEquals("{MAC=5E:FF:56:A2:AF:15}", gm.toMap().toString());
+  @Test
+  public void test010_HOSTPORT() throws Throwable {
 
-	}
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{HOSTPORT}");
 
-	@Test
-	public void test009_IPORHOST() throws Throwable{
+    Match gm = g.match("www.google.fr:80");
+    gm.captures();
+    assertEquals("{HOSTPORT=www.google.fr:80, IPORHOST=www.google.fr, PORT=80}", gm.toMap()
+        .toString());
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{IPORHOST}");
+  }
 
-		Match gm = g.match("www.google.fr");
-		gm.captures();
-		assertEquals("{IPORHOST=www.google.fr}", gm.toMap().toString());
+  @Test
+  public void test01_COMBINEDAPACHELOG() throws Throwable {
 
-	}
+    g.addPatternFromFile("patterns/patterns");
+    g.compile("%{COMBINEDAPACHELOG}");
 
-	@Test
-	public void test010_HOSTPORT() throws Throwable{
+    Match gm =
+        g.match("112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"");
+    gm.captures();
+    assertNotNull(gm.toJson());
+    // System.out.println(gm.toJson());
 
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{HOSTPORT}");
 
-		Match gm = g.match("www.google.fr:80");
-		gm.captures();
-		assertEquals("{HOSTPORT=www.google.fr:80, IPORHOST=www.google.fr, PORT=80}", gm.toMap().toString());
+    gm =
+        g.match("112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET /wp-content/plugins/easy-table/themes/default/style.css?ver=1.0 HTTP/1.1\" 304 - \"http://www.nflabs.com/\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"");
+    gm.captures();
+    assertNotNull(gm.toJson());
+    // System.out.println(gm.toJson());
 
-	}
+    // assertEquals("{HOSTPORT=www.google.fr:80, IPORHOST=www.google.fr, PORT=80}",
+    // gm.toMap().toString());
 
-	@Test
-	public void test01_COMBINEDAPACHELOG() throws Throwable{
-
-		g.addPatternFromFile("patterns/patterns");
-		g.compile("%{COMBINEDAPACHELOG}");
-
-		Match gm = g.match("112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"");
-		gm.captures();
-		assertNotNull(gm.toJson());
-		//System.out.println(gm.toJson());
-
-
-		gm = g.match("112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET /wp-content/plugins/easy-table/themes/default/style.css?ver=1.0 HTTP/1.1\" 304 - \"http://www.nflabs.com/\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"");
-			gm.captures();
-		assertNotNull(gm.toJson());
-		//System.out.println(gm.toJson());
-
-		//assertEquals("{HOSTPORT=www.google.fr:80, IPORHOST=www.google.fr, PORT=80}", gm.toMap().toString());
-
-	}
+  }
 
 }
