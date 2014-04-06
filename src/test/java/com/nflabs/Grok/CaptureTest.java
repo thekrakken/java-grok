04fa3c48f39666e719e559ebc7f7047b383d4739
anthony corbacho <corbacho.anthony@gmail.com>
2014-04-06 22:32:42 +0900

    Add nasted capture test

--- src/test/java/com/nflabs/Grok/CaptureTest.java
+++ src/test/java/com/nflabs/Grok/CaptureTest.java
@@ -3,12 +3,15 @@ package com.nflabs.Grok;
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import com.nflabs.grok.Grok;
 import com.nflabs.grok.GrokException;
 import com.nflabs.grok.Match;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CaptureTest {
 
   static Grok grok;
@@ -29,6 +32,32 @@ public class CaptureTest {
     m.captures();
     assertEquals(1, m.toMap().size());
     assertEquals("{foo=Hello World}", m.toMap().toString());
+  }
+  
+  @Test
+  public void test002_captureMathodMulti() throws GrokException {
+    grok.addPattern("foo", ".*");
+    grok.addPattern("bar", ".*");
+    grok.compile("%{foo} %{bar}");
+    Match m = grok.match("Hello World");
+    assertEquals("(?<name0>.*) (?<name1>.*)", grok.getExpandedPattern());
+    assertEquals("Hello World", m.line);
+    m.captures();
+    assertEquals(2, m.toMap().size());
+    assertEquals("{bar=World, foo=Hello}", m.toMap().toString());
+  }
+  
+  @Test
+  public void test003_captureMathodNasted() throws GrokException {
+    grok.addPattern("foo", "\\w+ %{bar}");
+    grok.addPattern("bar", "\\w+");
+    grok.compile("%{foo}");
+    Match m = grok.match("Hello World");
+    assertEquals("(?<name0>\\w+ (?<name1>\\w+))", grok.getExpandedPattern());
+    assertEquals("Hello World", m.line);
+    m.captures();
+    assertEquals(2, m.toMap().size());
+    assertEquals("{bar=World, foo=Hello World}", m.toMap().toString());
   }
 
 }
