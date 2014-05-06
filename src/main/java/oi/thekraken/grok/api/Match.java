23290805024575a6edc6a8de3ffeddc134114f2d
anthony corbacho <corbacho.anthony@gmail.com>
2014-05-06 16:16:30 +0900

    Add capture string and list to grok

--- src/main/java/oi/thekraken/grok/api/Match.java
+++ src/main/java/oi/thekraken/grok/api/Match.java
@@ -185,19 +185,40 @@ public class Match {
    * will return
    * {"IP":"127.0.0.1", "status":200}
    * </p>
+   * If pretty is set to true, json will return prettyprint json string.
    *
    * @return Json of the matched element in the text
    */
-  public String toJson() {
+  public String toJson(Boolean pretty) {
     if (capture == null)
       return "{}";
     if (capture.isEmpty())
       return "{}";
 
     this.cleanMap();
-    Gson gs = new GsonBuilder().setPrettyPrinting().create();// new Gson();
+    Gson gs;
+    if(pretty){
+     gs = new GsonBuilder().setPrettyPrinting().create();// new Gson();
+    } else {
+      gs = new Gson();
+    }
     return gs.toJson(/* cleanMap( */capture/* ) */);
 
+  }
+  
+  /**
+   * Get the json representation of the matched element
+   * <p>
+   * example:
+   * map [ {IP: 127.0.0.1}, {status:200}]
+   * will return
+   * {"IP":"127.0.0.1", "status":200}
+   * </p>
+   *
+   * @return Json of the matched element in the text
+   */
+  public String toJson() {
+    return toJson(false);
   }
 
   /**
