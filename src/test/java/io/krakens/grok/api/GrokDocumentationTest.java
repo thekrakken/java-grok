927c227208deeb52041e4118bcef9da9804cd7f2
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-04-06 21:11:05 -0400

    added capture keyset tests (#115)

--- src/test/java/io/krakens/grok/api/GrokDocumentationTest.java
+++ src/test/java/io/krakens/grok/api/GrokDocumentationTest.java
@@ -1,9 +1,15 @@
 package io.krakens.grok.api;
 
+import java.util.HashSet;
 import java.util.Map;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.*;
+
+import org.assertj.core.util.Arrays;
 
 public class GrokDocumentationTest {
 
@@ -27,5 +33,23 @@ public class GrokDocumentationTest {
     final Map<String, Object> capture = gm.capture();
 
     Assertions.assertThat(capture).hasSize(22);
+		final boolean debug = false;
+		final Object[] keywordArray = new Object[] { "COMBINEDAPACHELOG",
+				"COMMONAPACHELOG", "clientip", "ident", "auth", "timestamp", "MONTHDAY",
+				"MONTH", "YEAR", "TIME", "HOUR", "MINUTE", "SECOND", "INT", "verb",
+				"httpversion", "rawrequest", "request", "response", "bytes", "referrer",
+				"agent" };
+		if (debug)
+			capture.keySet().stream().forEach(System.err::println);
+		assertTrue(new HashSet<Object>(Arrays.asList(keywordArray))
+				.containsAll(new HashSet<Object>(capture.keySet())));
+
+		Arrays.asList(keywordArray).stream()
+				.forEach(o -> assertThat(capture.keySet(), hasItem((String) o)));
+		assertThat(new HashSet<Object>(capture.keySet()),
+				containsInAnyOrder(keywordArray));
+		assertTrue(new HashSet<Object>(capture.keySet())
+				.containsAll(new HashSet<Object>(Arrays.asList(keywordArray))));
+
   }
 }
